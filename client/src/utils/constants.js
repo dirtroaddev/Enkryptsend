1e02d26afe5ad2ee2698e16ca788419fe353474a
fullstackjunkie <rongoodwindev@gmail.com>
2022-01-01 23:19:30 -0500

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x2e4901f0B2645017d262053Fd4b3901f4D28e35f';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,16 @@
+//https://eth-ropsten.alchemyapi.io/v2/V8ol3gb4uCKv5AsS2srA-WNBR0JMKGqF
+
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/LR6TKYgSkz6vKUddJtepbr9ycDqRgXj2',
+      accounts: ['f213cb734decdf3558d34966f30bbebe63805d51288007df08d34bf4e4b4a685'],
+    },
+  },
+};
+
+
